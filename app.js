4abc0d61aa77fdc0da8a2ed5a487e4b22508eed1
Santiago Duré <santidure2002@gmail.com>
2024-06-04 15:24:56 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,49 @@
+document.addEventListener("DOMContentLoaded", (event) => {
+  // Manejador del primer formulario
+  const formPorcentaje = document.querySelector(".form_porcentaje");
+
+  formPorcentaje.addEventListener("submit", function (event) {
+    event.preventDefault(); // Prevenir el envío del formulario
+
+    const montoOriginal = parseFloat(
+      formPorcentaje.querySelector('input[placeholder="monto original"]').value
+    );
+    const montoActual = parseFloat(
+      formPorcentaje.querySelector('input[placeholder="monto actual"]').value
+    );
+
+    if (isNaN(montoOriginal) || isNaN(montoActual)) {
+      alert("Por favor, ingrese valores numéricos válidos.");
+      return;
+    }
+
+    const diferencia = montoActual - montoOriginal;
+    const diferenciaPorcentual = (diferencia / montoOriginal) * 100;
+
+    alert(`La diferencia porcentual es ${diferenciaPorcentual.toFixed(2)}%`);
+  });
+
+  // Manejador del segundo formulario
+  const formMontoFinal = document.querySelector(".form_montoFinal");
+
+  formMontoFinal.addEventListener("submit", function (event) {
+    event.preventDefault(); // Prevenir el envío del formulario
+
+    const montoOriginal = parseFloat(
+      formMontoFinal.querySelector('input[placeholder="monto original"]').value
+    );
+    const porcentajeSumar = parseFloat(
+      formMontoFinal.querySelector('input[placeholder="porcentaje a sumar"]')
+        .value
+    );
+
+    if (isNaN(montoOriginal) || isNaN(porcentajeSumar)) {
+      alert("Por favor, ingrese valores numéricos válidos.");
+      return;
+    }
+
+    const montoFinal = montoOriginal + montoOriginal * (porcentajeSumar / 100);
+
+    alert(`El monto final es ${montoFinal.toFixed()}`);
+  });
+});
